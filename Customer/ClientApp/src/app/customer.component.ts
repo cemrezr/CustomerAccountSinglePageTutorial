d950f3c99d12e3431c5ccc4e85e24f2fcca1fdf5
Cemre Zereyak <cemrezereyak@gmail.com>
2021-03-18 01:59:21 +0300

    add: customer component

---
+++ Customer/ClientApp/src/app/customer.component.ts
@@ -0,0 +1,24 @@
+import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Customer } from './models/customer';
+
+@Component({
+  selector: 'app-value',
+  templateUrl: './customer.component.html'
+})
+
+export class CustomerComponent implements OnInit {
+  constructor(private http: HttpClient) { }
+  customers: Customer[]=[];
+
+  ngOnInit() {
+
+    this.getCustomers().subscribe(data => {
+      this.customers=data
+    })
+  }
+
+  getCustomers() {
+    return this.http.get<Customer[]>("https://localhost:44372/api/customer")
+  }
+}
---
+++ Customer/ClientApp/src/app/models/customer.ts
@@ -0,0 +1,9 @@
+export class Customer {
+  id: number;
+  name: string;
+  lastName: string;
+  dateTime: Date;
+  birthCity: string;
+  ownerBranchCode: number;
+
+}
